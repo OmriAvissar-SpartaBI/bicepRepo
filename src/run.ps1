b5f5e2a3b6ea0f631ad1458615d526e0f6ab50f7
Omri Avissar <omria@spartabi.com>
2025-02-27 12:06:29 +0200

    עגכעגכ

--- src/run.ps1
+++ src/run.ps1
@@ -23,19 +23,18 @@ New-AzResourceGroupDeploymentStack @deployment
 $deployment = @{
     'Name' = 'myDeploymentStack'
     'ResourceGroupName' = 'bicep'
-    'TemplateFile' = 'C:\Users\Omri Avissar\OneDrive - SpartaBI Ltd\Desktop\bicep\deploy\main.bicep'
-    'ActionOnUnmanage' = 'DeleteAll'
+    'TemplateFile' = 'C:\Users\Omri Avissar\OneDrive - SpartaBI Ltd\Desktop\bicepRepo\bicepRepo\deploy\main.bicep'
     'DefaultProfile' = $azureProfile
-    'Description' = 'myDescription'
     'Force' = $true
     'SkipTemplateParameterPrompt' = $true
-    'DenySettingsMode' = 'None'
     'TemplateParameterObject' = @{
+        'userAssignedIdentityName' = 'UAMI-zzz'
     }
 }
 
 
 New-AzResourceGroupDeployment @deployment
+
 
 # Get-AzDeploymentScriptLog -ResourceGroupName 'bicep' -Name 'CopyConfigScript'
 
